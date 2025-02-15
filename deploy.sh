4f1c3e83bcc16a3d6f974f5d9fdc440203902317
Chris Mason <chris.mason@stocksoftware.com.au>
2025-02-15 15:58:57 +1100

    update Jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -17,15 +17,15 @@ echo "VER=$TAG_NAME"
 echo -e "TAG_NAME=$TAG_NAME" > env.txt
 
 echo "Force pulling..."
-rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -e env.txt -p expenseManager -f ${BASE_DIR}/docker-compose-${ENV_NAME}.yml pull
+rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -e env.txt -p expenseManager -f ${BASE_DIR}/docker-compose.${ENV_NAME}.yml pull
 
 echo "Starting deployment..."
-rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -r ${BASE_DIR}/rancher-compose.yml -e env.txt -p expenseManager -f ${BASE_DIR}/docker-compose-${ENV_NAME}.yml up --upgrade -d --pull --batch-size 1
+rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -r ${BASE_DIR}/rancher-compose.yml -e env.txt -p expenseManager -f ${BASE_DIR}/docker-compose.${ENV_NAME}.yml up --upgrade -d --pull --batch-size 1
 
 if [ $? -eq 0 ]; then
   echo "Deploy success! Confirming..."
-  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -e env.txt -p expenseManager -f ${BASE_DIR}/docker-compose-${ENV_NAME}.yml up --confirm-upgrade -d --batch-size 1
+  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -e env.txt -p expenseManager -f ${BASE_DIR}/docker-compose.${ENV_NAME}.yml up --confirm-upgrade -d --batch-size 1
 else
   echo "Deploy failed :( rolling back..."
-  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -e env.txt -p expenseManager -f ${BASE_DIR}/docker-compose-${ENV_NAME}.yml up --rollback -d --batch-size 1
+  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -e env.txt -p expenseManager -f ${BASE_DIR}/docker-compose.${ENV_NAME}.yml up --rollback -d --batch-size 1
 fi
